91f12c2257eb8a46964c3834fdfe024f0550c8ad
tonye <tonye.iyalla@yahoo.com>
2020-03-29 15:51:47 -0400

    Create square component

---
+++ tictacapp/src/app/square/square.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.scss']
+})
+export class SquareComponent  {
+
+  constructor() { }
+
+  
+
+}
